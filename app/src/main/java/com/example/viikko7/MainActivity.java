af69eac02eeec6dbebabf7eff993f83fb4be4fd9
ViliRaunola <raunolavili@gmail.com>
2020-02-22 11:13:42 +0200

    Toimiva

--- app/src/main/java/com/example/viikko7/MainActivity.java
+++ app/src/main/java/com/example/viikko7/MainActivity.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView teksti;
+    EditText txt;
+    String muuttuja;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         teksti = (TextView) findViewById(R.id.textView2);
+        txt = (EditText) findViewById(R.id.Syote);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String haeTeksti = txt.getText().toString();
+                teksti.setText(haeTeksti);
+            }
+        });
 
     }
 
-    public void Testi(View v) {
-        System.out.println("Hello World!");
-        teksti.setText("Hello World!");
-    }
 }
